8691593d10db1786e8e05eb4c5ec3cac442ba915
Keenan Payne <keenanpayne504@gmail.com>
2022-07-06 16:48:20 -0500

    styles element page

--- practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
+++ practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+async function createPage() {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search")
+    const data = await response.json()
+    const url = data[0].url
+    console.log(url)
+    console.log(data)
+    const urlparts = url.split("/")
+    const catbreed = urlparts[urlparts.length-1]
+    const img = document.createElement("img")
+    const div = document.createElement("div")
+    const fig = document.createElement("figure")
+    const figcaption = document.createElement("figcaption")
+    document.body.appendChild(div)
+    div.appendChild(fig)
+    img.setAttribute("src",url)
+    figcaption.innerText = catbreed
+    figcaption.setAttribute("id","figcaption")
+    fig.setAttribute("id","fig")
+    fig.appendChild(figcaption)
+    fig.appendChild(img)
+    document.getElementById("figcaption").style.textAlign = "center"
+    document.getElementById("fig").style.alignItems = "center"
+
+}
+
+window.onload = createPage
